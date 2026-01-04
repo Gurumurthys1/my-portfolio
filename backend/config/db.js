4304488c8b1b5bfa01295817d78ad421141d034d
Gurumurthys1 <gurumurthys.252005@gmail.com>
2026-01-04 20:24:38 +0530

    Fix Mongo URI and remove process exit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,11 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(1);
+    // process.exit(1); // Do not exit in serverless environment
   }
 };
 
