dde45cc2b45f7e50e20cd8dbd51eac4095ab4274
Gurumurthys1 <163425568+Gurumurthys1@users.noreply.github.com>
2026-01-20 23:42:16 +0530

    add

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // Use deployed backend for both Dev and Prod (Frontend-Only Mode)
-//const API_BASE_URL = 'https://my-portfolio-s2nv.onrender.com/api';
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://my-portfolio-s2nv.onrender.com/api';
+//const API_BASE_URL = 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
